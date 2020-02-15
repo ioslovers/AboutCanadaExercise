51f8811987413341d716132a9675da1857a5e4b0
Ashish Tripathi <tripathi6013@gmail.com>
2020-02-15 23:19:52 +1100

    Added unit test case of facts service and stub data

--- AboutCanadaExercise/AboutCanadaExerciseTests/AboutCanadaExerciseTests.swift
+++ AboutCanadaExercise/AboutCanadaExerciseTests/AboutCanadaExerciseTests.swift
@@ -11,17 +11,54 @@ import XCTest
 
 class AboutCanadaExerciseTests: XCTestCase {
 
+    var factsList: Facts?
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: "Facts", withExtension: "json"),
+            let data = try? Data(contentsOf: url) else {
+                return
+        }
+        
+        let decoder = JSONDecoder()
+        factsList = try? decoder.decode(Facts.self, from: data)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testProductionServiceRunning() {
+        // Put the code you want to measure the time of here.
+        setUp()
+        let expectation = self.expectation(description: "Get Facts abouts Canada's service is failed and we don't receive correct response")
+        Networking.fetchFactsAboutCanada { result in
+            switch result {
+            case .success(let data):
+                print(data.title!)
+                XCTAssertNotNil(data)
+                expectation.fulfill()
+            default:
+                XCTFail("Expected get facts service response with error json")
+            }
+        }
+        self.waitForExpectations(timeout: 600.0)
+    }
+    
+    func testServiceError() {
+        let expectation = self.expectation(description: "Looks like, latest facts is not on the top")
+        Networking.fetchFactsAboutCanada(shouldFail: true) { result in
+            switch result {
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            default:
+                XCTFail("Expected get facts service response with error json")
+
+            }
+        }
+        self.waitForExpectations(timeout: 6.0)
     }
 
     func testPerformanceExample() {
