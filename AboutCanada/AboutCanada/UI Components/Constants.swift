3bcde6f53e91ad1c8df5144a17a30d0b265b9734
Ashish Tripathi <ashish.tripathi@westpac.com.au>
2020-02-17 13:54:22 +1100

    Added localisable strings and constants

---
+++ AboutCanada/AboutCanada/UI Components/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  AboutCanada
+//
+//  Created by Ashish Tripathi on 17/2/20.
+//  Copyright © 2020 Ashish Tripathi. All rights reserved.
+//
+
+import Foundation
+
+public enum AccessibilityKey {
+    public static let cellIdentifier = "%@_cell"
+    public static let titleIdentifier = "%@_title"
+    public static let descriptionIdentifier = "%@_description"
+    public static let imageViewIdentifier = "%@_imageView"
+}
